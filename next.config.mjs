a52973a6431af8c853d876c7299b58187d9ee569
harshpatel-27 <harshpatel2782008@gmail.com>
2025-07-05 23:01:14 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         hostname: "cloud.appwrite.io",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-// next.config.ts
 import withPWA from "next-pwa";
-// import type { NextConfig } from "next";
-
-// const baseConfig: NextConfig = {
-//   // Your other Next.js config options here
-// };
-
 const nextConfig = withPWA({
   dest: "public",
   register: true,
