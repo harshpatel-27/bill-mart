7564c4e799a3dcf42c625f031f4dd6bb74005f20
harshpatel-27 <harshpatel2782008@gmail.com>
2025-07-05 22:54:52 +0530

    Delete src directory

--- src/schema/invoices.schema.ts
+++
@@ -1,6 +0,0 @@
-import * as z from "zod";
-
-export const CreateInvoiceSchema = z.object({
-  name: z.string().min(1, { message: "Category name is required" }),
-});
-export type CreateInvoiceSchemaType = z.infer<typeof CreateInvoiceSchema>;
--- src/schema/user-schema.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-
-export const SigninValidation = z.object({
-  email: z.string().email(),
-  password: z
-    .string()
-    .min(8, {message: "Password must be at least 8 characters."}),
-});
-export type SigninType = z.infer<typeof SigninValidation>;
