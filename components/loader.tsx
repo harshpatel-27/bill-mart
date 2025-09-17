07ff35c30c0b1ea04485cfa98f424ddd659af92b
harshpatel-27 <harshpatel2782008@gmail.com>
2025-09-17 23:12:57 +0530

    Delete components directory

--- components/loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "lucide-react";
-
-export const CustomLoader = () => {
-  return (
-    <div className="flex items-center justify-center flex-1 h-full">
-      <Loader className="animate-spin size-5" />
-    </div>
-  );
-};
